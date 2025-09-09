da51ddd0d526d95fbe458d45d3eb4bdcebbc3653
danielkamweru <daniel.kamweru@student.moringaschool.com>
2025-09-09 20:49:22 +0300

    code grade 1

--- codegrade1.js
+++ codegrade1.js
@@ -1,21 +1,24 @@
-function grade(marks){
-if (marks >79){
-    return "A";
-}else if (marks >=60 && marks <=79){
-    return "B";
-}else if (marks >=50 && marks <=59){
-    return "C";
-}else if (marks >=40 && marks <=49){
-    return "D";
-}else if (marks < 40){
-    return "E";
-}
-}
-document.getElementById("grade").addEventListener("click", function(event) {
-    event.preventDefault(); 
-    const marks = parseInt(document.getElementById("marks").value);
-    const result = grade(marks);
-    document.getElementById("result").innerText = `Grade: ${result}`;
-});
+let marks = parseInt(prompt("Enter student marks (0 - 100):"));
+
+    if (marks < 0 || marks > 100 || isNaN(marks)) {
+      alert("Invalid input! Marks should be between 0 and 100.");
+    } else {
+      let grade;
+
+      if (marks > 79) {
+        grade = "A";
+      } else if (marks >= 60) {
+        grade = "B";
+      } else if (marks >= 49) {
+        grade = "C";
+      } else if (marks >= 40) {
+        grade = "D";
+      } else {
+        grade = "E";
+      }
+
+      alert("The grade is: " + grade);
+    }
+
 
 
